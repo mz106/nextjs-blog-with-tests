02cd80b7fadc625c8170589af69881032d276304
Michael Z <michaelzwierzanski@gmail.com>
2022-05-27 12:49:54 +0100

    first commit/setup

---
+++ components/layout/Mainheader.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import classes from './mainHeader.module.css';
+
+const Mainheader = () => {
+  return (
+    <div>Mainheader</div>
+  )
+}
+
+export default Mainheader
